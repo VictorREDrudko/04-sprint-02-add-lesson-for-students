008dfaf11355c978013bf6d87f038e066becf345
Victor RED <viktor_RED_rudko@mail.ru>
2024-12-17 11:10:09 +0300

    init project

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { App } from './app/App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './app/store.ts'
 
+// 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <App />
